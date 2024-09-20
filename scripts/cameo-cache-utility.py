4ac9d45c1368509282fd187ac5d3f95ac9e9cf8f
Dakota <73153176+dakotatokad@users.noreply.github.com>
2024-09-20 17:58:16 -0600

    create python script

---
+++ scripts/cameo-cache-utility.py
@@ -0,0 +1,30 @@
+import os
+
+def main():
+    print("===============================================================================================================")
+    print("This script will give you the option to delete all offline projects and/or the local cache data for Cameo. For more information about this utility refer to the README.md file.")
+    print("")
+    print("*******************************************************************")
+    print("** Be sure to commit projects to TWC before running this script  **")
+    print("** as this script can remove ALL saved offline and cache data    **")
+    print("** from your machine.                                            **")
+    print("*******************************************************************")
+    print("")
+    print("===============================================================================================================")
+
+def getUserId():
+    print("Get user ID")
+
+def getCameoVersion():
+    print("Get cameo version")
+
+def buildCameoPath():
+    print("Build Path")
+
+def removeOfflineProjects():
+    print("Remove offline projects")
+
+def removeCache():
+    print("Remove cache")
+
+main()
