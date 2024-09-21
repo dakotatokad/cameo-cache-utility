cb526d37a7055f8577d48372c36d2861daf67758
Dakota <73153176+dakotatokad@users.noreply.github.com>
2024-09-20 21:46:30 -0600

    add archive functionality for cache

--- scripts/python/backupAndArchive.py
+++ scripts/python/backupAndArchive.py
@@ -1,21 +1,33 @@
 import shutil
 import os
 from datetime import date
-from signal import pause
 import time
 
+# Cameo path is expected as:
+# C:\Users\<USERNAME>\AppData\Local\.<modeling tool name>\<modeling tool version number>\
+# Offline: ..\restricted\offline
+# Cache: ..\cache
+
 def removeOfflineProjects(cameoPath):
+    print("Removing offline projects...")
     print("Creating a backup...")
     rootPath = os.path.join(cameoPath, 'projects')
     archiveName = os.path.join(cameoPath, 'archive', str(date.today()))
     shutil.make_archive(archiveName, "zip", rootPath)
     time.sleep(3)
+    print("Removing offline projects...")
+    # TODO confirm path is as expected (so as to not nuke the entire cameo directory)
+    # TODO force RMDIR on rootPath
     print('Offlines projects have been archived and removed...')
-    os.system('tree')
-    os.system('pause')
-
     
 def removeCache(cameoPath):
-    print("Path: ", cameoPath)
-
-removeOfflineProjects(cameoPath)
+    print("Removing cache files...")
+    print("Creating a backup...")
+    rootPath = os.path.join(cameoPath, 'cache')
+    archiveName = os.path.join(cameoPath, 'cache-archive', str(date.today()))
+    shutil.make_archive(archiveName, "zip", rootPath)
+    time.sleep(3)
+    print("Removing cache files...")
+    # TODO confirm path is as expected (so as to not nuke the entire cameo directory)
+    # TODO force RMDIR on rootpath
+    print("Cache files have been removed...")
