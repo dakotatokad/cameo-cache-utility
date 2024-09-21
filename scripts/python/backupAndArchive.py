d61be5e49700ad3bb02c7e968f55a0e1224f1d0a
Dakota <73153176+dakotatokad@users.noreply.github.com>
2024-09-20 21:32:47 -0600

    update archive function

--- scripts/python/backupAndArchive.py
+++ scripts/python/backupAndArchive.py
@@ -1,22 +1,18 @@
 import shutil
 import os
 from datetime import date
+from signal import pause
 import time
-
-cameoPath = os.path.join('D:\\git-repository', 'cameo-cache-utility', 'cameo-cache-utility', 'test-environment')
 
 def removeOfflineProjects(cameoPath):
     print("Creating a backup...")
-    rootPath = os.path.join(cameoPath, 'src')
+    rootPath = os.path.join(cameoPath, 'projects')
     archiveName = os.path.join(cameoPath, 'archive', str(date.today()))
     shutil.make_archive(archiveName, "zip", rootPath)
-
-    #shutil.make_archive(archive_name, "zip", root_dir)
-
-    # This archives the entire directory path instead of a specific directory and i can not understand why....
-    #shutil.make_archive(archive_name, "zip", root_dir=cameoPath, base_dir=cameoPath+"\\original")
-    #shutil.make_archive(date.today(), zip, cameoPath + "\\archive", cameoPath, False)
-    #time.sleep(3)
+    time.sleep(3)
+    print('Offlines projects have been archived and removed...')
+    os.system('tree')
+    os.system('pause')
 
     
 def removeCache(cameoPath):
