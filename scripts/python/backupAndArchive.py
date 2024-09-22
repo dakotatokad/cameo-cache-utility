f4e8b1dcb6b68eab131da127e8051864f5bb75c1
Dakota <help@arrakis.one>
2024-09-21 19:28:30 -0600

    add rmdir operations

--- scripts/python/backupAndArchive.py
+++ scripts/python/backupAndArchive.py
@@ -11,11 +11,13 @@ import time
 def removeOfflineProjects(cameoPath):
     print("Removing offline projects...")
     print("Creating a backup...")
+    # Append \projects directory onto root path for offline files
     rootPath = os.path.join(cameoPath, 'projects')
     archiveName = os.path.join(cameoPath, 'archive', str(date.today()))
     shutil.make_archive(archiveName, "zip", rootPath)
     time.sleep(3)
     print("Removing offline projects...")
+    shutil.rmtree(rootPath) # this needs to be tested...
     # TODO confirm path is as expected (so as to not nuke the entire cameo directory)
     # TODO force RMDIR on rootPath
     print('Offlines projects have been archived and removed...')
@@ -23,11 +25,13 @@ def removeOfflineProjects(cameoPath):
 def removeCache(cameoPath):
     print("Removing cache files...")
     print("Creating a backup...")
+    # Append \cache directory onto root path for offline files
     rootPath = os.path.join(cameoPath, 'cache')
     archiveName = os.path.join(cameoPath, 'cache-archive', str(date.today()))
     shutil.make_archive(archiveName, "zip", rootPath)
     time.sleep(3)
     print("Removing cache files...")
+    shutil.rmtree(rootPath) # this needs to be tested
     # TODO confirm path is as expected (so as to not nuke the entire cameo directory)
     # TODO force RMDIR on rootpath
     print("Cache files have been removed...")
